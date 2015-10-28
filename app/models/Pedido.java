02a6be03a7952d6de1413ce3f3066591e47c7ba6
Genesis0123 <bytemen11@gmail.com>
2015-10-27 23:17:30 -0500

    +modelos:comprobante y tarjeta1111

--- app/models/Pedido.java
+++ app/models/Pedido.java
@@ -1,5 +1,26 @@
 package models;
 
+import java.util.*;
+import javax.persistence.*;
+import com.avaje.ebean.Model;
+import play.data.validation.Constraints;
 
-public class Pedido {
+import javax.persistence.Entity;
+import javax.persistence.Table;
+//import javax.validation.Constraint;
+
+@Entity
+@Table(name = "pedido")
+public class Pedido extends Model{
+
+    @Id
+    private String codigo;
+
+    @ManyToOne
+    @JoinColumn(name = "codigoPersona", cascade=CascadeType.ALL)
+    private
+    List<Persona> personasList;
+
+
+
 }
